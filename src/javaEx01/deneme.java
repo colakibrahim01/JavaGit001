297506ecf4f0a8916831cd733a7c3b0626d8e8bf
colakibrahim01 <63121371+colakibrahim01@users.noreply.github.com>
2021-02-28 13:37:24 -0500

    Update deneme.java
    conflicting change on remote

--- src/javaEx01/deneme.java
+++ src/javaEx01/deneme.java
@@ -5,7 +5,7 @@ public class deneme {
 	public static void main(String[] args) {
 		//Creating a new Java class
 		System.out.println("Hello Everyone");
-		//here is the conflict of the remote main branch
+		//here is the conflict of the remote main branch online
 
 	}
 
